b02dc31d015846b835f6bdb13cc8de1cf47b2282
Figo Fan <figo.fan@arris.com>
2017-07-25 18:58:08 +0800

    added testng test case

---
+++ src/test/java/LibraryTestNGTest.java
@@ -0,0 +1,11 @@
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class LibraryTestNGTest {
+  @Test(groups = { "testng" })
+  public void f() {
+	  Library classUnderTest = new Library();
+      assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+  }
+}
